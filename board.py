bdb3b98b79396cafe901f37ad734e3365b2c7a38
IsaacPyle <isaac.a.pyle@gmail.com>
2021-12-20 09:06:31 -0700

    Added basic movement functionality, no checks for legal moves yet. Bot and Player classes not used, likely deleting Player and just using Bot in the future.

--- board.py
+++ board.py
@@ -1,6 +1,6 @@
 class Board():
     def __init__(self):
-        self.turn = "white"
+        self.whites_turn = True
         self.initial_state = [
             ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
             ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
@@ -13,30 +13,17 @@ class Board():
         ]
         self.board_state = self.initial_state
 
-# def setup():
-        # start_pieces = {"a1": pieces.Rook("a1", "white"), "b1": pieces.Knight("b1", "white"), 
-        #     "c1": pieces.Bishop("c1", "white"), "d1": pieces.Queen("d1", "white"), 
-        #     "e1": pieces.King("e1", "white"), "f1": pieces.Bishop("f1", "white"), 
-        #     "g1": pieces.Knight("g1", "white"), "h1": pieces.Rook("h1", "white"),
-        #     "a2": pieces.Pawn("a2", "white"), "b2": pieces.Pawn("b2", "white"), 
-        #     "c2": pieces.Pawn("c2", "white"), "d2": pieces.Pawn("d2", "white"),
-        #     "e2": pieces.Pawn("e2", "white"), "f2": pieces.Pawn("f2", "white"), 
-        #     "g2": pieces.Pawn("g2", "white"), "h2": pieces.Pawn("h2", "white"),
-        #     "a8": pieces.Rook("a8", "black"), "b8": pieces.Knight("b8", "black"), 
-        #     "c8": pieces.Bishop("c8", "black"), "d8": pieces.King("d8", "black"), 
-        #     "e8": pieces.Queen("e8", "black"), "f8": pieces.Bishop("f8", "black"), 
-        #     "g8": pieces.Knight("g8", "black"), "h8": pieces.Rook("h8", "black"),
-        #     "a7": pieces.Pawn("a7", "black"), "b7": pieces.Pawn("b7", "black"), 
-        #     "c7": pieces.Pawn("c7", "black"), "d7": pieces.Pawn("d7", "black"),
-        #     "e7": pieces.Pawn("e7", "black"), "f7": pieces.Pawn("f7", "black"), 
-        #     "g7": pieces.Pawn("g7", "black"), "h7": pieces.Pawn("h7", "black")}
-        # return start_pieces
+    def make_move(self, move):
+        self.board_state[move.start_row][move.start_col] = "--"
+        self.board_state[move.end_row][move.end_col] = move.moved_piece
+        self.whites_turn = not self.whites_turn
 
-    # def move(self, piece, initial_location, end_location):
-    #     temp = self.board_state.pop(initial_location)
-    #     self.board_state[end_location] = temp
-    #     if self.board_state[end_location]:
-    #         if self.turn == "white":
-    #             self.player.opponent_pieces.append(piece)
-    #         else:
-    #             self.bot.opponent_pieces.append(piece)
+class Move():
+    def __init__(self, board, start, end):
+        self.start_row = start[0]
+        self.start_col = start[1]
+        self.end_row = end[0]
+        self.end_col = end[1]
+        self.moved_piece = board[self.start_row][self.start_col]
+        self.captured_piece = board[self.end_row][self.end_col]
+
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pygame import color
-from pygame.constants import QUIT
+from pygame.constants import MOUSEBUTTONDOWN, QUIT
 import board
 import pygame as py
 
@@ -22,12 +22,29 @@ def main():
     bd = board.Board()
     load_initial_images()
     running = True
-    drawGame(screen, bd)
+    selected_square = ()
+    clicks = []
     while running:
         for e in py.event.get():
             if e.type == py.QUIT:
                 running = False
+            if e.type == MOUSEBUTTONDOWN:
+                loc = py.mouse.get_pos()
+                col = loc[0] // SQUARE_SIZE
+                row = loc[1] // SQUARE_SIZE
+                if selected_square == (row, col):
+                    selected_square == ()
+                    clicks == []
+                else:
+                    selected_square = (row, col)
+                    clicks.append(selected_square)
+                if len(clicks) == 2:
+                    move = board.Move(bd.board_state, clicks[0], clicks[1])
+                    bd.make_move(move)
+                    selected_square = ()
+                    clicks = []
 
+        drawGame(screen, bd)
         clock.tick(MAX_FPS)
         py.display.flip()
 
